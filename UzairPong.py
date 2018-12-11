01fbb91217998ff34c2a5cfdac9a269fea9d14c4
Uzair <umalik859@lsr7.net>
2018-12-11 09:02:27 -0600

    Today I finished the whole Pong game, but I have not done any modifications.

--- UzairPong.py
+++ UzairPong.py
@@ -5,6 +5,7 @@ sense = SenseHat()
 white = (255, 255, 255)
 blue = (0, 0, 255)
 
+
 bat_y = 4
 ball_position = [3,3]
 ball_velocity = [1,1]
@@ -12,9 +13,9 @@ ball_velocity = [1,1]
 
 # Functions
 def draw_bat():
-    sense.set_pixel(0, bat_y, white)
-    sense.set_pixel(0, bat_y + 1, white)
-    sense.set_pixel(0, bat_y - 1, white)
+    sense.set_pixel(0, bat_y, blue)
+    sense.set_pixel(0, bat_y + 1, blue)
+    sense.set_pixel(0, bat_y - 1, blue)
 
 def move_up(event):
     global bat_y
@@ -25,7 +26,19 @@ def move_down(event):
     global bat_y
     if event.action == 'pressed' and bat_y < 6:
         bat_y +=1
-
+def draw_ball():
+    sense.set_pixel(ball_position[0], ball_position[1], white)
+    ball_position[0] += ball_velocity[0]
+    if ball_position[0] == 7 or ball_position[0] == 0:
+        ball_velocity[0] = -ball_velocity[0]
+    ball_position[1] += ball_velocity[1]
+    if ball_position[1] == 7 or ball_position[1] == 0:
+        ball_velocity[1] = -ball_velocity[1]
+    if ball_position[0] == 1 and (bat_y - 1) <= ball_position[1] <= (bat_y + 1):
+        ball_velocity[0] = -ball_velocity[0]
+    if ball_position[0] == 0:
+        sense.show_message("You Lose",)
+    
 
 # Main program
 while True:
@@ -34,4 +47,7 @@ while True:
     sense.stick.direction_up = move_up
     sense.clear(0, 0, 0)
     sense.stick.direction_down = move_down
+    draw_ball()
+
+
 
